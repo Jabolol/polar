f55cc2d2cd4e2e82bd904a3363c8f8afe9d24657
Javier R <javier.rios-urbano@epitech.eu>
2023-10-09 13:06:05 +0200

    add: overriden `build_request` pure virtual method

--- include/polar.h
+++ include/polar.h
@@ -1,6 +1,7 @@
 #include <memory>
 #include <string>
 #include "include/mime.h"
+#include "server.h"
 
 #ifndef __POLAR_H__
     #define __POLAR_H__
@@ -15,13 +16,23 @@ typedef struct {
     std::string path;
 } meta_t;
 
-class Polar {
+class Polar : public Server {
   protected:
     Mime mime;
     options_t &options;
     std::shared_ptr<meta_t> meta;
+    std::string get_file_content(void);
     void serve_dir(void);
     void serve_file(void);
+    std::string build_request(void) noexcept override
+    {
+        std::string content = get_file_content();
+        std::string type = mime.get_mime(options.path);
+
+        return "HTTP/1.1 200 OK\r\nContent-Length:"
+            + std::to_string(content.length()) + "\r\nContent-Type: " + type
+            + "\r\n\r\n" + content;
+    }
 
   public:
     Polar(options_t &options);
