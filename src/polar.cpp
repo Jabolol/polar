dcc639d19e23a41b99b0805fb730bb901ee86411
Javier R <javier.rios-urbano@epitech.eu>
2023-10-09 13:07:20 +0200

    add: file serving `logic`

--- src/polar.cpp
+++ src/polar.cpp
@@ -1,23 +1,47 @@
 #include "include/polar.h"
+#include <fstream>
 #include <iostream>
 #include <memory>
+#include <stdexcept>
+#include <string>
 
-Polar::Polar(options_t &options) : options(options)
+Polar::Polar(options_t &options) : Server(options.port), options(options)
 {
     auto [port, path] = options;
 
-    this->meta = std::make_shared<meta_t>();
-    this->meta->path = options.path;
-    this->meta->is_dir = false;
+    meta = std::make_shared<meta_t>();
+    meta->path = options.path;
+    meta->is_dir = false;
 
     std::cout << "\x1B[2J\x1B[H";
     std::cout << port << " :: " << path + "\n" << std::endl;
 }
 
+std::string Polar::get_file_content(void)
+{
+    std::string path = options.path;
+    std::ifstream file(path);
+
+    try {
+        if (!file.is_open()) {
+            throw std::runtime_error("could not open file");
+        }
+        std::string content((std::istreambuf_iterator<char>(file)),
+            std::istreambuf_iterator<char>());
+        file.close();
+        return content;
+    } catch (std::runtime_error &e) {
+        return e.what();
+    }
+}
+
 void Polar::serve(void)
 {
-    std::string type = this->mime.get_mime(this->options.path);
-    std::cerr << "Content-Type: " + type << std::endl;
+    try {
+        start();
+    } catch (std::runtime_error &e) {
+        std::cerr << e.what() << std::endl;
+    }
 }
 
 Polar::~Polar()
