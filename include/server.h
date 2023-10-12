63ea94e006180608a3ba053acab09fb1fd12c237
Javier R <javier.rios-urbano@epitech.eu>
2023-10-12 20:57:40 +0200

    add: inherit methods from `Signal`

--- include/server.h
+++ include/server.h
@@ -1,17 +1,19 @@
-#include <string>
-
 #ifndef __SERVER_H__
-    #define __SERVER_H__
+#define __SERVER_H__
 
-class Server {
+#include <string>
+#include "signals.h"
+
+class Server : public Signal {
   private:
     int server_port;
     int server_socket;
+    static bool terminate_server;
     void handler(int client_socket);
 
   public:
     Server(int port);
-    bool start(void) noexcept(false);
+    bool start(void);
     virtual std::string build_request(void) noexcept = 0;
     virtual ~Server() = 0;
 };
