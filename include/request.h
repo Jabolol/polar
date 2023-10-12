78b3394a184a76393a5b3c59dcd5cacc4ddf1b11
Javier R <javier.rios-urbano@epitech.eu>
2023-10-12 20:57:21 +0200

    add: request `class`

---
+++ include/request.h
@@ -0,0 +1,20 @@
+#ifndef __REQUEST_H__
+#define __REQUEST_H__
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+class Request {
+  public:
+    Request(std::string request);
+    std::string path;
+    std::string method;
+    std::unordered_map<std::string, std::string> headers;
+    ~Request() = default;
+
+  private:
+    std::vector<std::string> split(std::string str, char delim);
+};
+
+#endif
---
+++ src/request.cpp
@@ -0,0 +1,43 @@
+#include "request.h"
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <vector>
+
+Request::Request(std::string request)
+{
+    if (request.empty()) {
+        return;
+    }
+
+    std::vector<std::string> lines = split(request, '\n');
+    std::vector<std::string> first_line = split(lines[0], ' ');
+
+    method = first_line[0];
+    path = first_line[1];
+
+    auto lower = [](std::string str) -> std::string {
+        std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+        return str;
+    };
+
+    for (unsigned int i = 1; i < lines.size(); i++) {
+        if (lines[i].empty() || lines[i] == "\r") {
+            break;
+        }
+        std::vector<std::string> header = split(lines[i], ':');
+        headers[lower(header[0])] = header[1];
+    }
+}
+
+std::vector<std::string> Request::split(std::string str, char delim)
+{
+    std::string token;
+    std::stringstream ss(str);
+    std::vector<std::string> parsed;
+
+    while (std::getline(ss, token, delim)) {
+        parsed.push_back(token);
+    }
+    return parsed;
+}
