328c8fd1bb365213e56cf519f0ff13e9ba6bfd46
Javier R <javier.rios-urbano@epitech.eu>
2023-10-12 20:57:09 +0200

    fix: simplify `class` members

--- include/polar.h
+++ include/polar.h
@@ -1,10 +1,9 @@
-#include <memory>
-#include <string>
-#include "include/mime.h"
-#include "server.h"
-
 #ifndef __POLAR_H__
-    #define __POLAR_H__
+#define __POLAR_H__
+
+#include <string>
+#include "mime.h"
+#include "server.h"
 
 typedef struct {
     int port;
@@ -20,24 +19,15 @@ class Polar : public Server {
   protected:
     Mime mime;
     options_t &options;
-    std::shared_ptr<meta_t> meta;
     std::string get_file_content(void);
     void serve_dir(void);
     void serve_file(void);
-    std::string build_request(void) noexcept override
-    {
-        std::string content = get_file_content();
-        std::string type = mime.get_mime(options.path);
-
-        return "HTTP/1.1 200 OK\r\nContent-Length:"
-            + std::to_string(content.length()) + "\r\nContent-Type: " + type
-            + "\r\n\r\n" + content;
-    }
+    std::string build_request(void) noexcept;
 
   public:
     Polar(options_t &options);
     void serve(void);
-    ~Polar();
+    ~Polar() = default;
 };
 
 #endif
