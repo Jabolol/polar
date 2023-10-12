4f110b532c362b7bf59c79e0469d98c4fa547721
Javier R <javier.rios-urbano@epitech.eu>
2023-10-12 20:59:58 +0200

    fix: refactor `signals` logic, update `errors`, print `request` data

--- src/server.cpp
+++ src/server.cpp
@@ -1,29 +1,26 @@
 #include "server.h"
-#include <atomic>
+#include <cerrno>
 #include <csignal>
+#include <cstring>
+#include <iostream>
+#include <memory>
 #include <netinet/in.h>
 #include <string>
 #include <sys/socket.h>
 #include <unistd.h>
+#include "request.h"
+#include "signals.h"
 
-std::atomic<bool> terminate_server(false);
+bool Server::terminate_server = false;
 
-void handle_signal(int signal)
+Server::Server(int port) : Signal(), server_port(port), server_socket(-1)
 {
-    if (signal == SIGINT) {
-        terminate_server.store(true);
-    }
-}
-
-Server::Server(int port) : server_port(port), server_socket(-1)
-{
-    struct sigaction sigbreak;
-    std::memset(&sigbreak, 0, sizeof sigbreak);
-    sigbreak.sa_handler = handle_signal;
-
-    if (sigaction(SIGINT, &sigbreak, NULL) != 0) {
-        throw std::runtime_error("Error setting up signal handler");
-    }
+    auto handler = [](int sig) -> void {
+        if (sig == SIGINT) {
+            Server::terminate_server = true;
+        }
+    };
+    register_signal(SIGINT, handler);
 }
 
 Server::~Server()
@@ -35,15 +32,26 @@ Server::~Server()
 
 void Server::handler(int client_socket)
 {
+    int len = 0;
+    socklen_t len_size = sizeof len;
+    getsockopt(client_socket, SOL_SOCKET, SO_RCVBUF, &len, &len_size);
+
+    std::shared_ptr<char[]> buffer(new char[len + 1]);
+    buffer[len] = '\0';
+    recv(client_socket, buffer.get(), len, MSG_PEEK);
+
+    Request request = Request(buffer.get());
+    std::cout << request.method << " ==> " << request.path << std::endl;
+
     std::string response = build_request();
     send(client_socket, response.c_str(), response.length(), 0);
 }
 
-bool Server::start(void) noexcept(false)
+bool Server::start(void)
 {
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket == -1) {
-        throw std::runtime_error("Error creating socket");
+        throw std::runtime_error(strerror(errno));
     }
 
     sockaddr_in server_addr{};
@@ -52,26 +60,26 @@ bool Server::start(void) noexcept(false)
     server_addr.sin_port = htons(server_port);
 
     if (bind(server_socket, (struct sockaddr *) &server_addr,
-            sizeof(server_addr))
+            sizeof server_addr)
         == -1) {
-        throw std::runtime_error("Error binding socket to port");
+        throw std::runtime_error(strerror(errno));
     }
 
     if (listen(server_socket, 5) == -1) {
-        throw std::runtime_error("Error listening for connections");
+        throw std::runtime_error(strerror(errno));
     }
 
-    while (!terminate_server.load()) {
+    while (!Server::terminate_server) {
         sockaddr_in client_addr{};
-        socklen_t client_len = sizeof(client_addr);
-        int clientSocket = accept(
+        socklen_t client_len = sizeof client_addr;
+        int client_socket = accept(
             server_socket, (struct sockaddr *) &client_addr, &client_len);
 
-        if (clientSocket == -1) {
+        if (client_socket == -1) {
             continue;
         }
-        handler(clientSocket);
-        close(clientSocket);
+        handler(client_socket);
+        close(client_socket);
     }
 
     close(server_socket);
